b2eeee990d52b4040fe0906d51f249ab9a835952
vamshiravula25 <109848175+vamshiravula25@users.noreply.github.com>
2025-05-08 18:11:02 +0530

    Delete backend/scrpt.py

--- backend/scrpt.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Download the latest version of the dataset
-path = kagglehub.dataset_download("therealeye/real-vs-fake-img")
-
-# Print the path where the dataset has been downloaded
-print("Path to dataset files:", path)
